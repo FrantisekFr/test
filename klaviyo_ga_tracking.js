08933aa266ff39411b91bce07c8785cfc0c8bd04
FrantisekFr <96049514+FrantisekFr@users.noreply.github.com>
2024-02-23 18:38:39 +0000

    Update klaviyo_ga_tracking.js

--- klaviyo_ga_tracking.js
+++ klaviyo_ga_tracking.js
@@ -1,19 +1,42 @@
-function handleDataLayerPush(event){  
-   console.log(event);
-   _learnq.push(['track', event.event, {
-    "payload":event.ecommerce.items
-  }]);
-}
+// function handleDataLayerPush(event){  
+//    console.log(event);
+//    _learnq.push(['track', event.event, {
+//     "payload":event.ecommerce.items
+//   }]);
+// }
 
-function initializeDataLayerListener() {
-   console.log('Hi from Universal Integration');
-    if (window.dataLayer && Array.isArray(window.dataLayer)) {
-        // Attach an event listener to the push event of the dataLayer array
-        window.dataLayer.addEventListener('push', function(event) {
-            // Handle the dataLayer push event
-            handleDataLayerPush(event);
-        });
-    }    
-}
+// function initializeDataLayerListener() {
+//    console.log('Hi from Universal Integration');
+//     if (window.dataLayer && Array.isArray(window.dataLayer)) {
+//         // Attach an event listener to the push event of the dataLayer array
+//         window.dataLayer.addEventListener('push', function(event) {
+//             // Handle the dataLayer push event
+//             handleDataLayerPush(event);
+//         });
+//     }    
+// }
 
-initializeDataLayerListener()
+// initializeDataLayerListener()
+
+
+(function() {
+    // Define the function to handle dataLayer push events
+    function handleDataLayerPush(event) {
+        console.log('dataLayer event received:', event);
+        _learnq.push(['track', event.event, {
+          "payload":event.ecommerce.items
+        }]);
+    }
+
+    // Attach an event listener to the push event of the dataLayer array
+    window.dataLayer.push = function() {
+        // Capture the arguments passed to the original push method
+        var args = Array.prototype.slice.call(arguments);
+        // Call the original push method
+        Array.prototype.push.apply(window.dataLayer, args);
+        // Extract the event object from the arguments
+        var event = args[0];
+        // Handle the dataLayer push event
+        handleDataLayerPush(event);
+    };
+})();
