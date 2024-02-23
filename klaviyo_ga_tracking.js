1470b2abdab1884f7d2c3cd7f3845173b1cfbe21
FrantisekFr <96049514+FrantisekFr@users.noreply.github.com>
2024-02-23 18:04:30 +0000

    Create klaviyo_ga_tracking.js

---
+++ klaviyo_ga_tracking.js
@@ -0,0 +1,17 @@
+function handleDataLayerPush(event){      
+   _learnq.push(['track', event.event, {
+    "payload":event.ecommerce.items
+  }]);
+}
+
+function initializeDataLayerListener() {
+    if (window.dataLayer && Array.isArray(window.dataLayer)) {
+        // Attach an event listener to the push event of the dataLayer array
+        window.dataLayer.addEventListener('push', function(event) {
+            // Handle the dataLayer push event
+            handleDataLayerPush(event);
+        });
+    }    
+}
+
+initializeDataLayerListener()
