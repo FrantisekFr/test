68dfdcadcd3991e2cb65322e083385c340b06830
FrantisekFr <96049514+FrantisekFr@users.noreply.github.com>
2024-03-01 14:09:54 +0000

    Update klaviyo_ga_tracking.js

--- klaviyo_ga_tracking.js
+++ klaviyo_ga_tracking.js
@@ -4,81 +4,49 @@
     // Handle dataLayer events
     function handleDataLayerPush(event) {
         console.log('dataLayer event detected by handler:', event);
-        
+
         var klaviyo = window.klaviyo || [];
-        // Validate supported event type and formatting        
+
+        var KLAVIYO_EVENT_KEY_MAP = {
+            "view_item": "Viewed Product",
+            "add_to_cart": "Added to Cart",
+            "begin_checkout": "Started Checkout",
+            "purchase": "Placed Order"
+        }
+
+        // Validate the main formatting expected for standard GA4 ecommerce events
         var eventName;
-        var ecommerceEvent;
         var ecommerceItems;
-        
+
         try {
             eventName = event.event;
-        } catch (err) {        
-        }
-
-        try {
-            ecommerceEvent = event.ecommerce;
-        } catch (err) {        
-        }
-
-        try {
             ecommerceItems = event.ecommerce.items;
-        } catch (err) {        
-        }
+        } catch (err) {}
 
         console.log(eventName);
-        console.log(ecommerceEvent);
         console.log(ecommerceItems);
-        
-        // Track standard events as relevant for Klaviyo assuming standard GA event formatting        
-        if(ecommerceEvent){
-            
-            switch (eventName && ecommerceEvent && ecommerceItems) {                    
-            
-                case 'view_item':
-                    ecommerce_items[0].$value = ecommerce_items[0].price;                    
-                    klaviyo.track("Viewed Product", ecommerce_items[0]);
-                    break;
-                
-                case 'add_to_cart':
-                    ecommerce_items[0].$value = ecommerce_items[0].price;                    
-                    klaviyo.track("Added to Cart", ecommerce_items);
-                    break;
-                
-                case 'begin_checkout':
-                    var checkoutValue = 0;
-                    var checkoutData = {};
 
-                    for (var i = 0; i < ecommerce_items.length; i++) {
-                        checkoutValue += ecommerce_items[i].price * ecommerce_items[i].quantity;
-                    };
-                    checkoutData.$value = parseFloat(checkoutValue.toFixed(2));
-                    checkoutData.Items = ecommerce_items;
-                    
-                    klaviyo.track("Started Checkout", checkoutData);
-                    break;
-                    
-                case 'purchase':
-                    var purchaseValue = 0;
-                    var purchaseData = {};
+        // Track select ecommerce events assuming standard GA4 ecommerce event formatting. 
+        if (ecommerceEvent && ecommerceItems) {
 
-                    for (var i = 0; i < ecommerce_items.length; i++) {
-                        purchaseValue += ecommerce_items[i].price * ecommerce_items[i].quantity;
-                    };
-                    purchaseData.$value = parseFloat(purchaseValue.toFixed(2));
-                    purchaseData.Items = ecommerce_items;
-                    
-                    klaviyo.track("Placed Order", purchaseData);
-                    break;
+            if (eventName == "view_item") {
+                ecommerce_items[0].$value = ecommerce_items[0].price;
+                klaviyo.track(KLAVIYO_EVENT_KEY_MAP.eventName, ecommerce_items[0]);
+            } else if (eventName == "add_to_cart") {
+                ecommerce_items[0].$value = ecommerce_items[0].price;
+                klaviyo.track(KLAVIYO_EVENT_KEY_MAP.eventName, ecommerce_items);
+            } else if (eventName == "begin_checkout" || eventName == "purchase")
+                var checkoutValue = 0;
+                var checkoutData = {};
 
-                default:
-                    // Track other ecommerce events as they stand i.e. without formatting the payload
-                    klaviyo.track(eventName, ecommerce_data);
+                for (var i = 0; i < ecommerce_items.length; i++) {
+                    checkoutValue += ecommerce_items[i].price * ecommerce_items[i].quantity;
+                };
+                checkoutData.$value = parseFloat(checkoutValue.toFixed(2));
+                checkoutData.Items = ecommerce_items;
+
+                klaviyo.track(KLAVIYO_EVENT_KEY_MAP.eventName, checkoutData);
             }
-        } 
-        //Track non-ecommerce events without payload formatting
-        else if (eventName && !ecommerceEvent) {            
-            klaviyo.track(eventName, event);
         }
     }
 
