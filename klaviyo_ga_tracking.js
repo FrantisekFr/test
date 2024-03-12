2966d9f263a80677a3508ea90195eb433f434628
FrantisekFr <96049514+FrantisekFr@users.noreply.github.com>
2024-03-12 11:00:45 +0000

    Update klaviyo_ga_tracking.js

--- klaviyo_ga_tracking.js
+++ klaviyo_ga_tracking.js
@@ -28,9 +28,11 @@
 
         // Track select ecommerce events assuming standard GA4 ecommerce event formatting. 
         if (eventName && ecommerceItems) {
-
+            
             if (eventName == "view_item") {
                 ecommerceItems[0].$value = ecommerceItems[0].price;
+                console.log('KL KEY EVENT MAP');
+                console.log(KLAVIYO_EVENT_KEY_MAP.eventName);
                 klaviyo.track(KLAVIYO_EVENT_KEY_MAP.eventName, ecommerceItems[0]);
             } else if (eventName == "add_to_cart") {
                 ecommerceItems[0].$value = ecommerceItems[0].price;
